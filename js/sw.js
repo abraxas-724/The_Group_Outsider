e79c133078edb4e0518ef03edb1e8f5f358359ee
Shi Yuhan <maranom520@gmail.com>
2025-10-12 13:37:26 +0800

    final version 9

--- js/sw.js
+++ js/sw.js
@@ -8,7 +8,7 @@
 
 // 缓存版本：每次需要强制客户端更新（包括音效文件、脚本）时递增
 // 建议语义：tgo-v<number>
-const CACHE_VERSION = 'tgo-v19';
+const CACHE_VERSION = 'tgo-v20';
 const CORE_CACHE = `${CACHE_VERSION}-core`;
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 
